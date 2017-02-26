3e9455e9853d719bdfe849e47765ff916749d2f7
Thomas Giulianelli <giuliat3@apps.tcnj.edu>
2017-02-26 11:52:38 -0500

    Formatted data; Added app icon.

--- app/src/main/java/com/giulianelli/heretostay/ScrollingActivity1.java
+++ app/src/main/java/com/giulianelli/heretostay/ScrollingActivity1.java
@@ -64,7 +64,7 @@ public class ScrollingActivity1 extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Coming Soon!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
